3faa08995f136a8db810963cc57e83ebd1efe2d7
LaunMatt <launaymatthew15@gmail.com>
2025-12-14 13:06:38 +0100

    Etape 2 : coder la partie back-office / Tâche 1 : gérer les formations

---
+++ src/Form/FormationType.php
@@ -0,0 +1,69 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+namespace App\Form;
+
+use App\Entity\Categorie;
+use App\Entity\Formation;
+use App\Entity\Playlist;
+use DateTime;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FormationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Titre'
+            ])
+            ->add('publishedAt', DateType::class, [
+                'widget' => 'single_text',
+                'data' => isset($options['data']) &&
+                    $options['data']->getPublishedAt() != null ? $options['data']->getPublishedAt() : new DateTime('now'),
+                'label' => 'Date de publication'
+            ])
+            ->add('playlist', EntityType::class, [
+                'class' =>Playlist::class,
+                'choice_label' => 'name',
+                'placeholder' => 'Aucune playlist',
+                'label' => 'Playlist'
+            ])
+            
+            ->add('categories', EntityType::class, [
+                'class'=> Categorie::class,
+                'choice_label'=> 'name',
+                'multiple'=> true,
+                'required'=> false,
+                'label' => 'Catégories'
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description',
+                'required' => false
+            ])
+            ->add('videoId', TextType::class, [
+                'label' => 'ID de la vidéo'
+            ])
+            ->add('submit', SubmitType::class, [
+                'label'=> 'Enregistrer'
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Formation::class,
+        ]);
+    }
+}
